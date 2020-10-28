34c2000a6c1cc04dd676b65979ec432bcab109b9
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2020-10-28 21:37:50 +0100

    Recreate toolchain from tarball if not present in tree
    Download tarball from SiFive and extract to exactly same location, where
    it was committed, just before attempting to make any further make rules.
    
    File .generated.linux.gtk.x86_64 is used as an indicator that toolchain
    is fine, so download and unpack is done only, if it's not there. This
    avoids redownloading on every make call.

--- make_scripts_riscv/toolchain.mk
+++ make_scripts_riscv/toolchain.mk
@@ -1,3 +1,22 @@
+CONFIG_TOOLPLATFORM=$(shell uname |cut -d '_' -f1)
+CONFIG_TOOLCHAIN_NAME=riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-centos6
+CONFIG_TOOLPATH=$(BL60X_SDK_PATH)/toolchain/riscv/$(CONFIG_TOOLPLATFORM)
 ##change to your toolchain path
-CONFIG_TOOLPREFIX ?= $(BL60X_SDK_PATH)/toolchain/riscv/$(shell uname |cut -d '_' -f1)/bin/riscv64-unknown-elf-
+CONFIG_TOOLPREFIX ?= $(CONFIG_TOOLPATH)/bin/riscv64-unknown-elf-
 #CONFIG_TOOLPREFIX ?= riscv64-unknown-elf-
+
+all: toolchain
+
+$(CONFIG_TOOLPATH)/.generated.linux.gtk.x86_64:
+	$(summary) WGET $(CONFIG_TOOLCHAIN_NAME).tar.gz
+	cd $(CONFIG_TOOLPATH) && wget https://static.dev.sifive.com/dev-tools/$(CONFIG_TOOLCHAIN_NAME).tar.gz
+	$(summary) TAR $(patsubst $(PWD)/%,%,$(CONFIG_TOOLCHAIN_NAME).tar.gz)
+	cd $(CONFIG_TOOLPATH) && tar -zxf $(CONFIG_TOOLCHAIN_NAME).tar.gz -C . --strip-components=1
+	$(summary) RM $(patsubst $(PWD)/%,%,$(CONFIG_TOOLCHAIN_NAME).tar.gz)
+	cd $(CONFIG_TOOLPATH) && rm $(CONFIG_TOOLCHAIN_NAME).tar.gz
+	$(summary) TOUCH $@
+	touch $@
+
+# use hidden file as indicator that toolchain is fine, but if it's not, try
+# downloading it from SiFive, unpack in place and remove tarball
+toolchain: $(CONFIG_TOOLPATH)/.generated.linux.gtk.x86_64
